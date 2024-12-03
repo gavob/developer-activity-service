1cbb50772e6f4200a44889b01aed98741b4dfe18
gavob <gavinbruce12@googlemail.com>
2024-12-03 11:07:48 +0000

    initial commit

---
+++ src/test/java/dev/gavin/devactivitydashboard/DevactivitydashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gavin.devactivitydashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevactivitydashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
