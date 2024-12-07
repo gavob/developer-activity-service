62ac0984f40c3fd8e683568870f1e22cb71978f8
gavob <gavinbruce12@googlemail.com>
2024-12-07 19:20:59 +0000

    Controller comments

--- src/main/java/dev/gavin/devactivitydashboard/controller/DevActivityController.java
+++ src/main/java/dev/gavin/devactivitydashboard/controller/DevActivityController.java
@@ -19,11 +19,22 @@ public class DevActivityController {
         this.gitHubApiService = gitHubApiService;
     }
 
+    /**
+     * GET endpoint for searching GitHub repositories
+     * @param searchTerm    String to search against repository names
+     * @return List of Repository Summaries
+     */
     @GetMapping("/search_repos/{search_term}")
     public List<RepositorySummary> searchRepos(@PathVariable("search_term") String searchTerm) {
         return gitHubApiService.searchPublicRepos(searchTerm);
     }
 
+    /**
+     * GET endpoint for returning repository data formatted for analysis
+     * @param owner     The name of the repository owner
+     * @param name      The name of the repository
+     * @return Repository Analysis
+     */
     @GetMapping("/repo_details/{owner}/{name}")
     public RepositoryAnalysis getRepositoryAnalysis(@PathVariable String owner, @PathVariable String name) {
         return gitHubApiService.getRepositoryAnalysis(owner, name);
